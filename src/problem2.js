226c98c7d31a4b76e84f9a52ed27bb0ce03c1783
cruellaDev <cruellaDevLee@gmail.com>
2023-09-24 21:36:58 +0900

    solved

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,19 @@
-function problem2(cryptogram) {
-  var answer;
-  return answer;
+function problem2(cryptogram = '') {
+    let cryptogramArray = cryptogram?.split('') || [];
+    for (let i = 0; i < cryptogram.length; i++) {
+        let deleteCount = 0;
+        cryptogramArray = cryptogramArray.reduce((acc, curr) => {
+            if (acc.at(-1) != curr) {
+                acc.push(curr);
+            } else {
+                acc.pop();
+                deleteCount += 1;
+            }
+            return acc;
+        }, []);
+        if (deleteCount == 0) break;
+    }
+    return cryptogramArray.join('');
 }
 
 module.exports = problem2;
