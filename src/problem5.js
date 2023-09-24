8e805fa0e258dacd7d94976f9ae3d77c075ee6b7
cruellaDev <cruellaDevLee@gmail.com>
2023-09-24 22:22:30 +0900

    solved

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,15 @@
 function problem5(money) {
-  var answer;
-  return answer;
+    let standards = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+    let answer = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    let standard, div, mod;
+    for (const i in standards) {
+        standard = standards[i];
+        div = Math.floor(money / standard);
+        mod = money % standard;
+        answer[i] = div;
+        money = mod;
+    }
+    return answer;
 }
 
 module.exports = problem5;
