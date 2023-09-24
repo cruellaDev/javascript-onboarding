e00026e87a558e7b936fefd26351eca4e8a7d07b
cruellaDev <cruellaDevLee@gmail.com>
2023-09-24 22:13:04 +0900

    solved

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,19 @@
-function problem4(word) {
-  var answer;
-  return answer;
+function problem4(word = '') {
+    let abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let zyx = "ZYXWVUTSRQPONMLKJIHGFEDCBA";
+    let answer = '';
+    for (const i in word) {
+        const upper = word[i]?.toUpperCase() || '';
+        const isUpperCase = word[i] === upper;
+        const index = abc.indexOf(upper);
+        if (index == -1) {
+            answer += word[i];
+            continue;
+        }
+        const change = zyx.at(index) || '';
+        answer += isUpperCase ? change : change.toLowerCase();
+    }
+    return answer;
 }
 
 module.exports = problem4;
